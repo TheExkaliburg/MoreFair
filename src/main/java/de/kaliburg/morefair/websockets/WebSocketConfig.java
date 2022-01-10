4fb4de02b379369e49d16cda04094680289cd0e2
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-01-10 10:32:18 +0100

    added allowed Origins

--- src/main/java/de/kaliburg/morefair/websockets/WebSocketConfig.java
+++ src/main/java/de/kaliburg/morefair/websockets/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/fairsocket").setHandshakeHandler(new CustomHandshakeHandler()).withSockJS();
+        registry.addEndpoint("/fairsocket").setHandshakeHandler(new CustomHandshakeHandler()).setAllowedOrigins("*").withSockJS();
     }
 
 
--- src/main/resources/static/fair/js/requests.js
+++ src/main/resources/static/fair/js/requests.js
@@ -12,6 +12,8 @@ async function connect() {
         login()
 
     })
+
+
 }
 
 function disconnect() {
