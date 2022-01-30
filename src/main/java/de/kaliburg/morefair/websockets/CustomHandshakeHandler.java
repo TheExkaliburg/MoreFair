c3218244ab70b99b4d1d268dc524f22b180b9b87
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-01-30 05:44:10 +0100

    even more debugging

--- src/main/java/de/kaliburg/morefair/websockets/CustomHandshakeHandler.java
+++ src/main/java/de/kaliburg/morefair/websockets/CustomHandshakeHandler.java
@@ -1,5 +1,6 @@
 package de.kaliburg.morefair.websockets;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
@@ -8,9 +9,13 @@ import java.security.Principal;
 import java.util.Map;
 import java.util.UUID;
 
+@Log4j2
 public class CustomHandshakeHandler extends DefaultHandshakeHandler {
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
-        return new StompPrincipal(UUID.randomUUID().toString());
+        UUID uuid = UUID.randomUUID();
+        log.info("Determining user for session {} as {}", request.getURI().toString(), uuid);
+
+        return new StompPrincipal(uuid.toString());
     }
 }
