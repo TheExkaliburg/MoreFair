89988110afd460dabed78a3b9a4240fdaa21c996
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-02-21 16:07:10 +0100

    100M as baseLadderCost

--- src/main/java/de/kaliburg/morefair/FairController.java
+++ src/main/java/de/kaliburg/morefair/FairController.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class FairController {
     public final static Integer MINIMUM_PEOPLE_FOR_PROMOTE = 10;
     // 250 Millionen
-    public final static BigInteger POINTS_FOR_PROMOTE = new BigInteger("250000000");
+    public final static BigInteger POINTS_FOR_PROMOTE = new BigInteger("100000000");
     // 500 Thousand
     public final static BigInteger BASE_VINEGAR_NEEDED_TO_THROW = new BigInteger("500000");
     // 2 Thousand
