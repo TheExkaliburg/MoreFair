969d4de83d4f58944238178bf32ea9355100b188
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-03-03 03:55:15 +0100

    Remove Typescript sample project

--- frontend/vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: (config) => {
-    config.optimization.minimize(false);
-  },
-});
