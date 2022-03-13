c4ceb9aaedf3144c5a9806fd0508dac0d795f97f
Lynn Schallenberg <Schallenberg0815@gmail.com>
2022-03-13 14:23:53 +0100

    Added first real options

--- frontend/src/options/store/optionsModule.js
+++ frontend/src/options/store/optionsModule.js
@@ -1,13 +1,27 @@
-import { BoolOption, RangeOption } from "../entities/option";
+import { BoolOption, RangeOption, IntegerOption } from "../entities/option";
 
 const optionsModule = {
   namespaced: true,
   state: () => {
     return {
       options: [
-        new BoolOption({ name: "show_chat", value: true }),
-        new BoolOption({ name: "dispense_cookies", value: false }),
-        new RangeOption({ name: "cookie_count", value: 69, min: 0, max: 100 }),
+        new BoolOption({
+          displayName: "Show all rankers",
+          name: "showAllRankers",
+          value: false,
+        }),
+        new RangeOption({
+          displayName: "Rankers at top",
+          name: "rankersAtTop",
+          value: 10,
+          min: 0,
+          max: 100,
+        }),
+        new IntegerOption({
+          displayName: "Rankers padding",
+          name: "rankersPadding",
+          value: 1000,
+        }),
       ],
     };
   },
@@ -17,13 +31,17 @@ const optionsModule = {
       //TODO: load locally
       //TODO: load from server
     },
-    updateOption(state, { option, value }) {
+    updateOption(state, { option, payload }) {
       const index = state.options.findIndex((o) => o.name === option.name);
       if (index >= 0) {
-        state.options[index].value = value;
+        state.options[index].set(payload);
       } else {
         throw new Error(`Option ${option.name} not found`);
       }
+      //This is always guaranteed to be the new value
+      //payload may also include other properties that are describing other aspects of the option
+      //eslint-disable-next-line no-unused-vars
+      const newValue = payload.value;
       //TODO: save locally
       //TODO: save to server
     },
--- frontend/src/views/OptionsView.vue
+++ frontend/src/views/OptionsView.vue
@@ -2,8 +2,13 @@
   <div>
     <div v-for="option in options" :key="option">
       <CheckBox v-if="option instanceof BoolOption" :option="option" />
-      <div v-if="option instanceof RangeOption">
-        {{ option.name }} {{ option.value }}
+      <RangeSlider v-else-if="option instanceof RangeOption" :option="option" />
+      <NumberInput
+        v-else-if="option instanceof NumberOption"
+        :option="option"
+      />
+      <div v-else>
+        Unimplemented Option {{ option.name }} {{ option.value }}
       </div>
     </div>
   </div>
@@ -11,9 +16,15 @@
 
 <script setup>
 import CheckBox from "@/options/components/CheckBox";
+import RangeSlider from "@/options/components/RangeSlider";
+import NumberInput from "@/options/components/NumberInput";
 import { computed } from "vue";
 import { useStore } from "vuex";
-import { BoolOption, RangeOption } from "@/options/entities/option";
+import {
+  BoolOption,
+  RangeOption,
+  NumberOption,
+} from "@/options/entities/option";
 
 const store = useStore();
 const options = computed(() => store.state.options.options);
