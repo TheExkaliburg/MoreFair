c6b55bc995c6966b4a0949c88cc5b3a91d8f5087
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-04-25 18:01:38 +0200

    Adding dropdown for moderator only options

--- frontend/src/chat/components/ChatMessage.vue
+++ frontend/src/chat/components/ChatMessage.vue
@@ -15,7 +15,17 @@
         }}</strong>
       </div>
       <div class="col-3 message-date">{{ msg.timeCreated }}</div>
-      <div class="col-1 message-options"></div>
+      <div
+        v-if="accessRole === 'OWNER' || accessRole === 'MODERATOR'"
+        class="col-1 message-options"
+      >
+        <a data-bs-toggle="dropdown">...</a>
+        <ul class="dropdown-menu">
+          <li>
+            <a class="dropdown-item" href="#" @click="onChange">test</a>
+          </li>
+        </ul>
+      </div>
     </div>
     <div class="row py-0 message-body">
       <ChatMessageBody :msg="msg" />
@@ -30,6 +40,7 @@ import ChatMessageBody from "@/chat/components/ChatMessageBody";
 
 const store = useStore();
 const settings = computed(() => store.state.settings);
+const accessRole = computed(() => store.state.user.accessRole);
 
 defineProps({
   msg: Object,
