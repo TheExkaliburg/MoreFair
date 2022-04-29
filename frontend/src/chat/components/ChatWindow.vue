TheExkaliburg/MoreFair

commit af80fadeb80bec2f212d2b24454e39974fa0f3bf

Can paste anything into chad, including pictures. Doesn't send. We just remove all nodes that are not text, span or br because we only use those. We then (lazily) check for pasted spans and check them to convert them to text. Fixes #46

frontend/src/chat/components/ChatWindow.vue
@@ -143,6 +143,26 @@ onMounted(() => {
             oldValue: "",
           });
         } else {
+          if (mutation.addedNodes.length > 0) {
+            //validate any potential added spans
+            mutation.addedNodes.forEach(function (node) {
+              if (node.tagName === "SPAN") {
+                if (node.classList.contains("mention")) {
+                  onElementChange({
+                    target: node,
+                    type: "characterData",
+                    oldValue: "",
+                  });
+                  console.info(node.innerText);
+                }
+              } else {
+                //convert it to a text node
+                const textNode = document.createTextNode(node.innerText);
+                node.parentNode.replaceChild(textNode, node);
+                console.info(node.innerText);
+              }
+            });
+          }
           //If the chat box was cleared, we need to remove the mention elements
           if (document.getElementById("chatInput").innerText.trim() === "") {
             let oldDropdown = document.getElementById("mentionDropdown");
@@ -153,6 +173,18 @@ onMounted(() => {
         }
       }
     });
+    //Remove all nodes that are not text nodes, spans, or brs
+    const msgBox = document.getElementById("chatInput");
+    const children = msgBox.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      if (
+        children[i].nodeType !== 3 &&
+        children[i].tagName !== "SPAN" &&
+        children[i].tagName !== "BR"
+      ) {
+        msgBox.removeChild(children[i]);
+      }
+    }
   });
   let config = {
     attributes: true,

commit c54ac5af014fed10c09bca2ebb31916b0662428a

Fixing a bug that only affects firefox. This was caused by af80fadeb80bec2f212d2b24454e39974fa0f3bf

frontend/src/chat/components/ChatWindow.vue
@@ -146,6 +146,9 @@ onMounted(() => {
           if (mutation.addedNodes.length > 0) {
             //validate any potential added spans
             mutation.addedNodes.forEach(function (node) {
+              if (node.tagName === "BR") {
+                return;
+              }
               if (node.tagName === "SPAN") {
                 if (node.classList.contains("mention")) {
                   onElementChange({
@@ -159,7 +162,7 @@ onMounted(() => {
                 //convert it to a text node
                 const textNode = document.createTextNode(node.innerText);
                 node.parentNode.replaceChild(textNode, node);
-                console.info(node.innerText);
+                console.log(node, node.innerText);
               }
             });
           }