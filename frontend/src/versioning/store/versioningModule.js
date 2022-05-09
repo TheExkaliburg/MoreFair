19a3d3641428b6ae38c674b6164eb94b97e0c2af
TheExkaliburg <34279353+TheExkaliburg@users.noreply.github.com>
2022-05-09 15:54:55 +0000

    Update versioningModule.js

--- frontend/src/versioning/store/versioningModule.js
+++ frontend/src/versioning/store/versioningModule.js
@@ -11,7 +11,7 @@ const versioningModule = {
             "Load themes from URLs or load previously saved themes.",
             "You can also delete themes you don't like.",
             "You can now hide you vinegar and grape count.",
-            "There will noe be an ETA until your vinegar is enough to be thrown.",
+            "There will now be an ETA until your vinegar is enough to be thrown.",
             "You can now hide your Chat.",
           ],
           improvements: [
