83f43f4e6fdb34e68dcc42decb8443e9f80666af
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-06-19 17:12:19 +0200

    Fixing the Versioning Module

--- frontend/src/versioning/store/versioningModule.js
+++ frontend/src/versioning/store/versioningModule.js
@@ -5,19 +5,24 @@ const versioningModule = {
   state: () => {
     return {
       versions: [
+        new Version("PATCH", "", {}),
         new Version("PATCH", "Fixing Asshole-Badge Limit", {
           features: [
             "An old known sound now chimes whenever you promoted.",
             "Another sound now rings when you reached the top of a ladder",
           ],
+          fixes: [
+            "Shown Symbol is capped at the highest available Symbol",
+            "Adding 15 new Symbols (may change at a later date)",
+          ],
+        }),
+        new Version("PATCH", "", {
           improvements: [
             "ETA to Top is now showing the eta until you could promote instead of the eta to the first place",
           ],
           fixes: [
             "Trying to fix that you can scroll the username in the chat-messages.",
             "Fixing the eta under The auto-promote to always show infinity.",
-            "Shown Symbol is capped at the highest available Symbol",
-            "Adding 15 new Symbols (may change at a later date)",
           ],
         }),
         new Version("PATCH", "", {
