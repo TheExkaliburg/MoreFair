1a8596167726b97b06d924b0d0c23bed7f256d06
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-06-19 16:50:01 +0200

    Quickfix for the IndexOutOfBounds Exception

--- backend/src/main/java/de/kaliburg/morefair/moderation/data/ModChatMessageData.java
+++ backend/src/main/java/de/kaliburg/morefair/moderation/data/ModChatMessageData.java
@@ -2,26 +2,28 @@ package de.kaliburg.morefair.moderation.data;
 
 import de.kaliburg.morefair.FairController;
 import de.kaliburg.morefair.chat.Message;
+import java.time.format.DateTimeFormatter;
 import lombok.Data;
 import org.apache.commons.text.StringEscapeUtils;
 
-import java.time.format.DateTimeFormatter;
-
 @Data
 public class ModChatMessageData {
-    private final String message;
-    private final String username;
-    private final String assholeTag;
-    private final Long accountId;
-    private final String timeCreated;
-    private final Integer ladderNumber;
 
-    public ModChatMessageData(Message message) {
-        this.message = StringEscapeUtils.unescapeJava(message.getMessage());
-        this.username = StringEscapeUtils.unescapeJava(message.getAccount().getUsername());
-        this.assholeTag = FairController.ASSHOLE_TAGS.get(message.getAccount().getTimesAsshole());
-        this.accountId = message.getAccount().getId();
-        this.timeCreated = message.getCreatedOn().format(DateTimeFormatter.ofPattern("EE HH:mm"));
-        this.ladderNumber = message.getLadder().getNumber();
-    }
+  private final String message;
+  private final String username;
+  private final String assholeTag;
+  private final Long accountId;
+  private final String timeCreated;
+  private final Integer ladderNumber;
+
+  public ModChatMessageData(Message message) {
+    this.message = StringEscapeUtils.unescapeJava(message.getMessage());
+    this.username = StringEscapeUtils.unescapeJava(message.getAccount().getUsername());
+    this.assholeTag = FairController.ASSHOLE_TAGS.get(
+        Math.min(message.getAccount().getTimesAsshole(),
+            FairController.ASSHOLE_TAGS.size() - 1));
+    this.accountId = message.getAccount().getId();
+    this.timeCreated = message.getCreatedOn().format(DateTimeFormatter.ofPattern("EE HH:mm"));
+    this.ladderNumber = message.getLadder().getNumber();
+  }
 }
