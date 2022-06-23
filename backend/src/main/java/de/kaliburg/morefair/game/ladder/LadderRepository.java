d8ae8c5f8f756455d407bdfffecf44514fbae37a
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-06-23 22:29:39 +0200

    forgot this somehow

--- backend/src/main/java/de/kaliburg/morefair/game/ladder/LadderRepository.java
+++ backend/src/main/java/de/kaliburg/morefair/game/ladder/LadderRepository.java
@@ -1,9 +1,20 @@
 package de.kaliburg.morefair.game.ladder;
 
+import de.kaliburg.morefair.game.round.RoundEntity;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LadderRepository extends JpaRepository<LadderEntity, Long> {
 
+  @Query("select l from LadderEntity l where l.uuid = :uuid")
+  Optional<LadderEntity> findByUuid(@Param("uuid") UUID uuid);
+
+  @Query("select l from LadderEntity l where l.round = :round and l.number = :number")
+  Optional<LadderEntity> findByRoundAndNumber(
+      @Param("round") RoundEntity round, @Param("number") Integer number);
 }
