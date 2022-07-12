a44daabd1ec26f7b12451cf396867ba565e72ad6
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-07-12 11:49:35 +0200

    Using vue final Modal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,5 +6,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vfmPlugin from "vue-final-modal";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(vfmPlugin).mount("#app");
