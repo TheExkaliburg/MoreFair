c6c84b7f03aa7990c5d76750436951839f183c49
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-07-13 22:16:22 +0200

    hotfix to have 2 less ladders

--- backend/src/main/java/de/kaliburg/morefair/FairController.java
+++ backend/src/main/java/de/kaliburg/morefair/FairController.java
@@ -29,7 +29,7 @@ public class FairController {
   public final static BigInteger BASE_GRAPES_NEEDED_TO_AUTO_PROMOTE = new BigInteger("5000");
   public final static Integer AUTO_PROMOTE_LADDER = 1;
   public final static Integer MANUAL_PROMOTE_WAIT_TIME = 30;
-  public final static Integer BASE_ASSHOLE_LADDER = 10;
+  public final static Integer BASE_ASSHOLE_LADDER = 8;
   public final static Integer ASSHOLES_FOR_RESET = 10;
   public final static List<String> ASSHOLE_TAGS = new ArrayList<>(Arrays.asList(
       "",
