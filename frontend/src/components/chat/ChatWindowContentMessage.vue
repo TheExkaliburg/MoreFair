9ee67fd7daa8a96a59f1d6bf30edf6d3c47412a2
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-10-27 20:00:59 +0200

    feat(frontend): adding a lot of tailwind remodeling of the current page

---
+++ frontend/src/components/chat/ChatWindowContentMessage.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="">Message</div>
+</template>
---
+++ frontend/src/components/chat/ChatWindowContentMessageBody.vue
@@ -0,0 +1,108 @@
+<template>
+  <span>
+    <span v-for="part in messageParts" :key="part">
+      <!-- Plain Message -->
+      <span v-if="part.is(MessagePartType.plain)">{{ part.text }}</span>
+      <!-- UserMentions -->
+      <span
+        v-else-if="part.is(MessagePartType.mentionUser)"
+        class="text-text-light"
+        >@{{ part.text }}
+      </span>
+      <sub
+        v-else-if="part.is(MessagePartType.mentionUserId)"
+        class="text-text-dark"
+        >#{{ part.text }}
+      </sub>
+      <!-- GroupMentions -->
+      <span v-else-if="part.is(MessagePartType.mentionGroup)"
+        ><span class="text-text-dark">$</span>{{ part.text
+        }}<span class="text-text-dark">$</span>
+      </span>
+      <!-- Unimplemented -->
+      <span v-else>
+        {{ part.text }}
+      </span>
+    </span>
+  </span>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  message: {
+    type: Object,
+    required: true,
+  },
+});
+
+enum MessagePartType {
+  plain,
+  mentionUser,
+  mentionUserId,
+  mentionGroup,
+}
+
+class MessagePart {
+  public readonly type: MessagePartType;
+  public readonly text: string;
+
+  constructor(type, text) {
+    this.type = type;
+    this.text = text;
+  }
+
+  is(type) {
+    if (this.type === type) return true;
+  }
+}
+
+const messageParts = computed<MessagePart[]>(() => {
+  let message = props.message.message;
+  const metadata = props.message.metadata;
+  const result: MessagePart[] = [];
+
+  if (!metadata) {
+    return [new MessagePart(MessagePartType.plain, message)];
+  }
+
+  const mentions = metadata.filter((m) => "u" in m && "id" in m && "i" in m);
+  const groupMentions = metadata.filter((m) => "g" in m && "i" in m);
+  const combinedMentions = [...mentions, ...groupMentions].sort(
+    (a, b) => a.i - b.i
+  );
+
+  let lastIndex = 0;
+  combinedMentions.forEach((m) => {
+    const isGroup = Boolean(m.g);
+    const index = m.i;
+    if (isGroup) {
+      let name = m.g;
+      name = name.trim();
+      if (message.slice(index, index + 3) !== "{§}") return;
+
+      result.push(
+        new MessagePart(MessagePartType.plain, message.slice(lastIndex, index))
+      );
+      result.push(new MessagePart(MessagePartType.mentionGroup, name));
+      lastIndex = index + 3;
+      message = message.slice(lastIndex);
+    } else {
+      const name = m.u.trim();
+      const id = m.id;
+      if (message.slice(index, index + 3) !== "{@}") return;
+
+      result.push(
+        new MessagePart(MessagePartType.plain, message.slice(lastIndex, index))
+      );
+      result.push(new MessagePart(MessagePartType.mentionUser, name));
+      result.push(new MessagePart(MessagePartType.mentionUserId, id));
+      lastIndex = index + 3;
+      message = message.slice(lastIndex);
+    }
+  });
+
+  return result;
+});
+</script>
