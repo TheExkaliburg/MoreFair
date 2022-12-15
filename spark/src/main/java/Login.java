0874a6cde1e8dc6d6c3bc3e0ac605ca8384d2f46
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2022-12-15 07:43:13 +0100

    feat(spark): groupBy account._id

--- spark/src/main/java/Login.java
+++ spark/src/main/java/Login.java
@@ -5,13 +5,16 @@ import org.apache.spark.sql.SparkSession;
 public class Login {
     public static void main(String[] args) {
         SparkSession spark = SparkSession.builder()
+                .master("local")
                 .appName("MongoSparkConnectorIntro")
                 .config("spark.mongodb.read.connection.uri", "mongodb://localhost/test.login")
                 .config("spark.mongodb.write.connection.uri", "mongodb://localhost/test.loginResults")
                 .getOrCreate();
 
-        Dataset<Row> df = spark.read().format("mongodb").load();//.as(Encoders.bean(/*LoginEntity
-        // .class*/));
-        df.show();
+        Dataset<Row> df = spark.read().format("mongodb").load();
+
+        Dataset<Row> result = df.groupBy("account._id").count();
+
+        result.show();
     }
 }
