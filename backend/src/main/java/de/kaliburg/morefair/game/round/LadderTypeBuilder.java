a9a779f687375a9dbd8cdb342f439456b49cba97
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2023-01-09 13:49:46 +0100

    fix(backend): error handeling of the Ladder- and RoundTypeBuilder should fix double assholePoints

--- backend/src/main/java/de/kaliburg/morefair/game/round/LadderTypeBuilder.java
+++ backend/src/main/java/de/kaliburg/morefair/game/round/LadderTypeBuilder.java
@@ -157,31 +157,42 @@ public class LadderTypeBuilder {
   }
 
   private Map<Float, LadderType> createInverseLookupTable(Map<LadderType, Float> weights) {
-    Map<Float, LadderType> inverseLookupTable = new HashMap<>();
-    float currentWeight = 0;
-    for (Map.Entry<LadderType, Float> entry : weights.entrySet()) {
-      if (entry.getValue() <= 0) {
-        continue;
+    try {
+      Map<Float, LadderType> inverseLookupTable = new HashMap<>();
+      float currentWeight = 0;
+      for (Map.Entry<LadderType, Float> entry : weights.entrySet()) {
+        if (entry.getValue() <= 0) {
+          continue;
+        }
+        currentWeight += entry.getValue();
+        inverseLookupTable.put(currentWeight, entry.getKey());
       }
-      currentWeight += entry.getValue();
-      inverseLookupTable.put(currentWeight, entry.getKey());
+      return inverseLookupTable;
+    } catch (Exception e) {
+      log.error("Error creating inverse lookup table", e);
+      Map<Float, LadderType> inverseLookupTable = new HashMap<>();
+      inverseLookupTable.put(1.f, LadderType.DEFAULT);
+      return inverseLookupTable;
     }
-    return inverseLookupTable;
   }
 
   private LadderType getRandomLadderType(Map<LadderType, Float> weights, String categoryName) {
-    float totalWeight = weights.values().stream().reduce(0.f, Float::sum);
-    float randomNumber = random.nextFloat(totalWeight);
-    List<Entry<Float, LadderType>> inverseLookupEntries = createInverseLookupTable(
-        weights).entrySet().stream().sorted(Entry.comparingByKey()).toList();
+    try {
+      float totalWeight = weights.values().stream().reduce(0.f, Float::sum);
+      float randomNumber = random.nextFloat(totalWeight);
+      List<Entry<Float, LadderType>> inverseLookupEntries = createInverseLookupTable(
+          weights).entrySet().stream().sorted(Entry.comparingByKey()).toList();
 
-    log.info("Random {} percentage for L{}: {}/{}", categoryName, ladderNumber, randomNumber,
-        totalWeight);
-    for (Map.Entry<Float, LadderType> entry : inverseLookupEntries) {
-      log.info("Checking {} percentage: {}/{}", entry.getValue(), entry.getKey(), totalWeight);
-      if (randomNumber < entry.getKey()) {
-        return entry.getValue();
+      log.info("Random {} percentage for L{}: {}/{}", categoryName, ladderNumber, randomNumber,
+          totalWeight);
+      for (Map.Entry<Float, LadderType> entry : inverseLookupEntries) {
+        log.info("Checking {} percentage: {}/{}", entry.getValue(), entry.getKey(), totalWeight);
+        if (randomNumber < entry.getKey()) {
+          return entry.getValue();
+        }
       }
+    } catch (Exception e) {
+      log.error("Error getting random ladder type", e);
     }
     return LadderType.DEFAULT;
   }
