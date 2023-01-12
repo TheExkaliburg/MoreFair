a8a8374dd8e855900157d7484449092cdf632732
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2023-01-12 16:39:25 +0100

    fix(spark): changing the config for which collection to pull

--- spark/src/main/java/MongoConnector.java
+++ spark/src/main/java/MongoConnector.java
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession;
 
 public class MongoConnector {
   public static Dataset<Row> read(SparkSession spark, String collection) {
-    return spark.read().format("mongodb").option("collection", collection).load();
+    return spark.read().format("mongodb").option("spark.mongodb.read.collection", collection).load();
   }
 
   public static String translateProfileToDatabase(String activeProfile) {
