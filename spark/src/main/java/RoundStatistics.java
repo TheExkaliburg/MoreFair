4788229264343a7976ee26145599415917a42391
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2023-02-05 13:17:08 +0100

    feat(spark): including the username of the account_id

--- spark/src/main/java/RoundStatistics.java
+++ spark/src/main/java/RoundStatistics.java
@@ -25,52 +25,60 @@ public class RoundStatistics {
     SparkSession spark = SparkUtils.createSparkSession();
     SqlConnector sqlConnector = new SqlConnector(spark);
 
-    /*
-    Dataset<Row> round = sqlConnector.query("SELECT * FROM round WHERE id = " + roundId).distinct();
+    Dataset<Row> account = sqlConnector.query("SELECT DISTINCT(account.*) FROM account "
+        + "INNER JOIN ranker on ranker.account_id = account.id "
+        + "INNER JOIN ladder on ranker.ladder_id = ladder.id "
+        + "WHERE ladder.round_id = " + roundId);
+    Dataset<Row> round =
+        sqlConnector.query("SELECT DISTINCT(round.*) FROM round "
+            + "WHERE id = " + roundId);
     Dataset<Row> roundType =
-        sqlConnector.query("SELECT * from round_type WHERE round_entity_id = " + roundId).distinct();
-    Dataset<Row> ladder = sqlConnector.query("SELECT * from ladder WHERE round_id = " + roundId ).distinct();
+        sqlConnector.query("SELECT DISTINCT(round_type.*) from round_type "
+            + "WHERE round_entity_id = " + roundId);
+    Dataset<Row> ladder = sqlConnector.query("SELECT DISTINCT(ladder.*) from ladder "
+        + "WHERE round_id = " + roundId );
     Dataset<Row> ladderType =
-        sqlConnector.query("SELECT ladder_type.* from ladder_type "
+        sqlConnector.query("SELECT DISTINCT(ladder_type.*) from ladder_type "
             + "INNER JOIN ladder ON ladder_type.ladder_entity_id = ladder.id "
-            + "WHERE ladder.round_id = " + roundId ).distinct();
-
-     */
-    Dataset<Row> ranker = sqlConnector.query("SELECT ranker.* from ranker "
+            + "WHERE ladder.round_id = " + roundId );
+    Dataset<Row> ranker = sqlConnector.query("SELECT DISTINCT(ranker.*) from ranker "
         + "INNER JOIN ladder ON ranker.ladder_id = ladder.id "
-        + "WHERE ladder.round_id = " + roundId).distinct();
-    /*
-    Dataset<Row> rankerUnlocks = sqlConnector.query("SELECT ranker_unlocks.* from ranker_unlocks "
+        + "WHERE ladder.round_id = " + roundId);
+    Dataset<Row> rankerUnlocks =
+        sqlConnector.query("SELECT DISTINCT(ranker_unlocks.*) from ranker_unlocks "
         + "INNER JOIN ranker ON ranker_unlocks.id = ranker.id "
         + "INNER JOIN ladder ON ranker.ladder_id = ladder.id "
-        + "WHERE ladder.round_id = " + roundId).distinct();
+        + "WHERE ladder.round_id = " + roundId);
 
-     */
 
-    Dataset<Row> promotedRankers = ranker.filter("growing = false")
-        .select("account_id", "ladder_id", "rank");
-
+    Dataset<Row> promotedRankers = ranker.filter("growing = false");
 
     WindowSpec window = Window.partitionBy("ladder_id").orderBy(col("rank").desc());
     Dataset<Row> promotedRankerPoints = promotedRankers
+        .select("account_id", "ladder_id", "rank")
         .withColumn("dense_rank",
           functions.dense_rank().over(window))
         .withColumn("promotion_points",
           greatest(expr("10 - dense_rank + 1"), lit(0))
     );
 
-
     Dataset<Row> rankerPointsPerLadder = promotedRankerPoints.groupBy("account_id")
         .agg(
-            //first("account_id").as("account_id"),
             collect_list("promotion_points").as("points"),
             sum("promotion_points").as("total")
         )
         .filter("total > 0")
         .sort(col("total").desc());
+    rankerPointsPerLadder = rankerPointsPerLadder
+        .join(account, rankerPointsPerLadder.col("account_id")
+            .equalTo(account.col("id")))
+        .select("account_id", "username", "points", "total");
 
     rankerPointsPerLadder.show(100);
     rankerPointsPerLadder.printSchema();
+  }
 
+  public static Dataset<Row> resultStatistics(int roundId, Dataset<Row> rankerPointsPerLadder) {
+    return null;
   }
 }
