1a4c8514f2b7ab36b32ebc53a436a1af81dbf2cd
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2023-07-02 19:09:04 +0200

    feat(frontend): mods being able to see other chads

--- frontend/src/components/chat/ChatWindowHeader.vue
+++ frontend/src/components/chat/ChatWindowHeader.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-end items-center text-button-text">
     <PaginationButtonGroup
       :current="chatStore.state.number"
-      :max="accountStore.state.highestCurrentLadder"
+      :max="maxChat"
       :prefix="'Chad'"
       class="h-8 w-42 self-end bg-background z-2"
       @change="(number) => chatStore.actions.changeChat(number)"
@@ -14,7 +14,15 @@
 import PaginationButtonGroup from "../../components/interactables/PaginationButtonGroup.vue";
 import { useChatStore } from "~/store/chat";
 import { useAccountStore } from "~/store/account";
+import { useRoundStore } from "~/store/round";
 
 const chatStore = useChatStore();
 const accountStore = useAccountStore();
+const roundStore = useRoundStore();
+
+const maxChat = computed<number>(() =>
+  accountStore.getters.isMod
+    ? roundStore.state.autoPromoteLadder + 1
+    : accountStore.state.highestCurrentLadder
+);
 </script>
