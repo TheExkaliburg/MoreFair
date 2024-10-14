0372f4497e1e7bee2178ad2e5239d281ccf17a90
Kali <kalifair@pm.me>
2024-10-14 18:25:57 +0200

    chore(migration): rename exisitng ladder_types in db

---
+++ backend/src/main/resources/db/migration/03/00.03/V03.00.03_001__update_ladder_type_names.sql
@@ -0,0 +1,11 @@
+UPDATE ladder_type
+SET types = 'LAVA'
+WHERE types = 'DROUGHT';
+
+UPDATE ladder_type
+SET types = 'TAXES'
+WHERE types = 'STINGY';
+
+UPDATE ladder_type
+SET types = 'VIRUS'
+WHERE types = 'NO_HANDOUTS';
